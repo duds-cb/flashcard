190af4d1cfeb17919b8f696a11779f5eebf8cf4f
duds-cb <cruz.bento@escola.pr.gov.br>
2025-08-14 15:12:14 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,41 @@
+criaCartao(
+'biologia',
+'Qual é o maior orgão do corpo humano?',
+'A pele'
+)
+
+criaCartao(
+    'biologia',
+    'Qual é o maior orgão interno do corpo humano',
+    'O intestino delgado'
+)
+
+criarCartão(
+    'matemática', 
+ 'Qual é o valor de Pi (aproximadamente)?', 
+ '3.14159'
+)
+
+criarCartão(
+ 'história', 
+ 'Em que ano o Brasil foi descoberto?',
+  '1500'
+)
+
+
+ criarCartão(
+    'geografia', 
+    'Qual é a capital da França?', 
+    'Paris'
+)
+
+criarCartão(
+    'química', 
+    'Qual é a fórmula da água?',
+     'H2O'
+     )
+
+criarCartao(
+    'Qual é a lei de Newton descreve a inércia?',
+    'primeira lei de Newton'
+)
